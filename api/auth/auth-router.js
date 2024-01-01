979b5e79eeb67a523872b1ebeaac401aeca8e138
Arron Alsop <alsop.arron@gmail.com>
2024-01-01 10:09:26 -0800

    'wip'

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,7 +8,7 @@ const User = require('../users/users-model')
 router.post("/register", validateRoleName, (req, res, next) => {
   const { username, password } = req.body
   const { role_name } = req
-  const hash = bcrypt.hashSync(password, 12)
+  const hash = bcrypt.hashSync(password, 8)
   User.add({ username, password: hash, role_name })
     .then(newRegistar => {
       res.status(201).json(newRegistar)
